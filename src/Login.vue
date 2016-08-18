8748c147beae784fe589613597ab5c023ac5f08d
Fareez Ahamed <fareez.ahamed@gmail.com>
2016-08-18 23:24:18 +0530

    Login box added

---
+++ src/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row login-box">
+      <div class="col-sm-4 col-sm-offset-4">
+        <div class="well well-lg">
+          <form action="/auth/login" method="POST" class="form-horizontal" role="form">
+            <div class="form-group">
+              <div class="col-sm-12">
+                <input type="email" name="email" id="inputEmail" class="form-control" value="" required="required" title="">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-sm-12">
+                <input type="password" name="password" id="inputPassword" class="form-control" value="" required="required" title="">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-sm-12">
+                <button type="submit" class="btn btn-block btn-primary">Login</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+import Login from './Login'
 import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss'
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
-  components: { App }
+  components: { App, Login }
 })
