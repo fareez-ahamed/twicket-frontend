60642a60d3806009d9fe39f3e44b2144b3b8d5e5
Fareez Ahamed <fareez.ahamed@gmail.com>
2016-08-20 15:55:29 +0530

    Dashboard view added

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container navbar-view-margin">
+    <div class="well">
+      <h1>Dashboard Here</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.navbar-view-margin {
+  margin-top: 100px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import App from './App'
 import Login from './Login'
 import NavbarView from './components/NavbarView'
+import Dashboard from './components/Dashboard'
 import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss'
 
 Vue.use(VueRouter)
@@ -14,7 +15,12 @@ router.map({
     component: Login
   },
   '/': {
-    component: NavbarView
+    component: NavbarView,
+    subRoutes: {
+      '/': {
+        component: Dashboard
+      }
+    }
   }
 })
 
